831750e0232686d397bee7a7e52063ce32e2ef45
wlfrd18 <wilfriedpanol@gmail.com>
2026-04-03 17:28:41 +0200

    fix the script of the task 0

--- web_application_security/0x0b_web_application_fast_incident_response/0-attack_ip.sh
+++ web_application_security/0x0b_web_application_fast_incident_response/0-attack_ip.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grep -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' logs.txt | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2}' 
+awk '{print $1}' logs.txt | sort | uniq -c | sort -nr | head -1 | awk '{print $2}'
