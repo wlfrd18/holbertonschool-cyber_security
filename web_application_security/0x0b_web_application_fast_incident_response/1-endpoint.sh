468ad44525d67cef90d6dbcbf53d6787cd7eb830
wlfrd18 <wilfriedpanol@gmail.com>
2026-04-03 17:09:42 +0200

    fix the script of the task 1

--- web_application_security/0x0b_web_application_fast_incident_response/1-endpoint.sh
+++ web_application_security/0x0b_web_application_fast_incident_response/1-endpoint.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grep -Eo 'POST [^ ]|GET [^ ]+' logs.txt | sort | uniq -c | sort -nr | head -n 1 | awk '{print $3}'
+awk '{print $1}' logs.txt | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2}'
