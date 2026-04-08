fc5a9349d1befd31ee4552bf569907f09f09f892
wlfrd18 <wilfriedpanol@gmail.com>
2026-04-08 02:12:55 +0200

    fix the script of the task 2

--- web_application_security/0x0c_web_application_foresics/2-accounts.sh
+++ web_application_security/0x0c_web_application_foresics/2-accounts.sh
@@ -1,2 +1,32 @@
 #!/bin/bash
-tail -1000 auth.log | grep "Accepted password" | awk '{print $9}' | sort -u
+tail -1000 $1 | awk '
+/Failed password for/ {
+	for (i = 1; i <= NF; i++)
+	{
+		if ($i == "for")
+		{
+			if ($(i + 1) == "invalid")
+				user = $(i + 3)
+			else
+				user = $(i + 1)
+			failed[user]++
+		}
+	}
+}
+/Accepted password for/ || /Accepted publickey for/ {
+	for (i = 1; i <= NF; i++)
+	{
+		if ($i == "for")
+		{
+			user = $(i + 1)
+			success[user]++
+		}
+	}
+}
+END {
+	for (user in success)
+	{
+		if (failed[user] > 0)
+			print user
+	}
+}'
