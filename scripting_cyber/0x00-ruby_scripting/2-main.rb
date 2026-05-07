e6083518915d6f401e2cf16f783c02f6cd326d4d
wlfrd18 <wilfriedpanol@gmail.com>
2026-05-07 07:56:43 -0400

    add the task 2

---
+++ scripting_cyber/0x00-ruby_scripting/2-main.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+
+require_relative '2-prime'
+
+puts prime(5)
+puts prime(6)
+puts prime(7)
+puts prime(8)
+puts prime(9)
+puts prime(10)
+puts prime(101)
---
+++ scripting_cyber/0x00-ruby_scripting/2-prime.rb
@@ -0,0 +1,8 @@
+#!/bin/bash/env ruby
+
+require 'prime'  #Import the Prime class from Ruby's library
+
+def prime(number)
+	Prime.prime?(number)  #Use the prime? method from the Prime class to check primality
+end
+
